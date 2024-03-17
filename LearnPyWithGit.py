948c76de390202e557d0be1ebc3a38f5e7bfd102
yeswanthkumarmedidhi <yeswanth599@gmail.com>
2024-03-17 17:47:19 +0900

    Adding my First Python program to GitHub Date:20240317

---
+++ LearnPyWithGit.py
@@ -0,0 +1,22 @@
+class LearnPyWithGit:
+    def __init__(self, name, address, phone_number, email_id):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email_id = email_id
+
+    def print_user_address(self):
+        print("requested user details: ")
+        print(self.name)
+        print(self.address)
+        print(self.phone_number)
+        print(self.email_id)
+
+
+print("Enter User Details")
+input_name = input("Enter User Name: ")
+input_address = input("Enter User Address: ")
+input_phone_number = input("Enter User PhoneNumber: ")
+input_email_id = input("Enter User email id: ")
+learnpy_with_gitobj = LearnPyWithGit(input_name, input_address, input_phone_number, input_email_id)
+learnpy_with_gitobj.print_user_address()
