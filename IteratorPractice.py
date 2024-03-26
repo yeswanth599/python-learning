b6887eed309be0137dcb021aec82f234f50e89f0
yeswanthkumarmedidhi <yeswanth599@gmail.com>
2024-03-26 22:08:30 +0900

    # Generators & Iterators #Date:2024/03/26 #Author:Yeswanth

--- IteratorPractice.py
+++ IteratorPractice.py
@@ -21,8 +21,17 @@ class IteratorPractice:
         print(next(my_iterator))
         print(next(my_iterator))
 
+    def generator_p(self):
+        i = 0
+        while (i < len(self.data_list)):
+            yield self.data_list[i]
+            i = i + 1
+
 
 data_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 iterator_p_obj = IteratorPractice(data_list)
 iterator_p_obj.for_loop_p()
 iterator_p_obj.iterator_p()
+data_list_res = iterator_p_obj.generator_p()
+print("Generator Output")
+print(data_list_res.__next__())
